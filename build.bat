54a43b1b4a9f01c79b813a05408270dcc0576438
Igor Quintaes <igor.quintaes@fatec.sp.gov.br>
2017-05-25 23:35:31 -0300

    publish to generate a new project exe and zip using msbuild and bat

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo iniciando processos && echo.
-
-@echo Build CORE
-"C:\Program Files (x86)\MSBuild\14.0\Bin\Msbuild.exe" "TibiaTools.sln" /property:Configuration=Release
-
-@echo processo finalizados
-
-pause
